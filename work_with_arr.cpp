d365c4c5598127ccf64ba9e793089f8599e356f8
gitUser <gitUser@gmail.com>
2024-12-05 21:20:52 +0300

    academi_work

--- work_with_arr.cpp
+++ work_with_arr.cpp
@@ -1,23 +1,29 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+#include <random>
+#include <cstring>
 
-void show_arr(int ARR[8], int size = 8) 
+// fill_arr_function
+
+void fill_arr(int* ARR, int size)
+{
+    for (int i = 0; i < size; i++) {
+        ARR[i] = (rand() % 101 * (-1)) % 101 * (-1);
+    }
+}
+
+// show_arr_function
+
+void show_arr(int* ARR, int size) 
 {
     for (int i = 0; i < size; i++) {
         std::cout << ARR[i] << ", ";
     }
 }
-
+    
 int main()
 {
-    const int size = 8;
-    int ARR[size];
-
-    for (int i = 0; i < size; i++) {
-        ARR[i] = size - i;
-    }
-    for (int i = 0; i < 8; i++) {
-        std::cout << ARR[i] << ", ";
-    }
-    std::cout << (char)8 << (char)8;
+    const int SIZE = 1000;
+    int ARR[SIZE];
+    fill_arr(ARR, SIZE);
+    show_arr(ARR, SIZE);
 }
