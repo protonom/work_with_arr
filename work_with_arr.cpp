074d5f0a5a4ee5b8ba82511e56c83f9db2f8a2a1
gitUser <gitUser@gmail.com>
2024-12-10 21:26:01 +0300

    ssss

--- work_with_arr.cpp
+++ work_with_arr.cpp
@@ -1,43 +1,79 @@
 ﻿#include <iostream>
-#include <random>
-#include <cstring>
+using std::cout;
 
-// fill_arr_function
-
-void fill_arr(int* ARR, int size)
+// Print all array elements
+// and nothing else
+void show_array(int* m, int s)
 {
-    for (int i = 0; i < size; i++) 
-    {
-        ARR[i] = rand() % 100;
+    int i;
+    for (i = 0; i < s; i++) {
+        cout << m[i] << ", ";
     }
+    cout << (char)8 << (char)8 << "\n";
+
 }
 
-// show_arr_function
+// Fill array function
+void fill_array(int* m, int sz)
+{
+    for (int i = 0; i < sz; i++) {
+        m[i] = rand() % 100;
 
-void show_arr(int* ARR, int size) 
-{
-    for (int i = 0; i < size; i++) 
-    {
-        std::cout << ARR[i] << ", ";
     }
+
 }
-int* sortirate(int* ARR, int size) 
+
+
+
+//Another sort fuction
+void sort2(int* m, int sz)
 {
+    int i, j;
+    int min;
+    for (i = 0; i < sz - 1; i++)
+    {
+        min = 101;
+        for (j = i; j < sz - 1; j++) {
+            if (m[j] < min) {
+                min = m[j];
+            }
+        }
+        // Нашли минимум и надо его поменять с i-м
+
+    }
+
+
+}
+
+//bublle sort function
+
+int* bublle_sort1(int* m, int sz) {
     int i = 0;
-    for (i = 0; i < size; i++) 
-    {
-        if (ARR[i] > ARR[i + 1]) {
-            ARR[i] = ARR[i + 1];
+    int reg = 0;
+    bool flag = true;
+    while (flag == true) {
+        flag = false;
+        for (i = 0; i < sz - 1; i++) {
+            if (m[i] > m[i + 1]) {
+                reg = m[i];
+                m[i] = m[i + 1];
+                m[i + 1] = reg;
+                flag = true;
+            }
         }
     }
-    return ARR;
+    return m;
 }
-    
+
+
 int main()
 {
-    const int SIZE = 10;
-    int ARR[SIZE];
-    fill_arr(ARR, SIZE);
-    show_arr(ARR, SIZE);
-    sortirate(ARR, SIZE);
+    const int SIZE = 50;
+    int mas[SIZE];
+    // Fill array
+    fill_array(mas, SIZE);
+    show_array(mas, SIZE);
+    bublle_sort1(mas, SIZE);
+    show_array(mas, SIZE);
 }
+
