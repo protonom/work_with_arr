cd3cd4717ab5b781e1639e8548a326d61d48307d
gitUser <gitUser@gmail.com>
2024-12-10 21:22:20 +0300

    third_work

--- work_with_arr.cpp
+++ work_with_arr.cpp
@@ -6,8 +6,9 @@
 
 void fill_arr(int* ARR, int size)
 {
-    for (int i = 0; i < size; i++) {
-        ARR[i] = (rand() % 101 * (-1)) % 101 * (-1);
+    for (int i = 0; i < size; i++) 
+    {
+        ARR[i] = rand() % 100;
     }
 }
 
@@ -15,15 +16,28 @@ void fill_arr(int* ARR, int size)
 
 void show_arr(int* ARR, int size) 
 {
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) 
+    {
         std::cout << ARR[i] << ", ";
     }
+}
+int* sortirate(int* ARR, int size) 
+{
+    int i = 0;
+    for (i = 0; i < size; i++) 
+    {
+        if (ARR[i] > ARR[i + 1]) {
+            ARR[i] = ARR[i + 1];
+        }
+    }
+    return ARR;
 }
     
 int main()
 {
-    const int SIZE = 1000;
+    const int SIZE = 10;
     int ARR[SIZE];
     fill_arr(ARR, SIZE);
     show_arr(ARR, SIZE);
+    sortirate(ARR, SIZE);
 }
